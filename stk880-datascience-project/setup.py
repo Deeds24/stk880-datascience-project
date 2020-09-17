572636344b597f072544183a5588eb8d69147028
Deeds24 <deidrebredenkamp@gmail.com>
2020-09-17 15:28:03 +0200

    first commit

---
+++ stk880-datascience-project/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A cookiecutter project we will be doing my first stk 880 project',
+    author='Deidre',
+    license='',
+)
